9d73f33a9362564b1f43e82c5a7fd913985193c4
Aditya Maheshwari <aditya.maheshwari@einfochips.com>
2026-03-09 18:20:17 +0530

    Initial implementation of core quiz functionality(9-3-2026)

---
+++ ReactJs Assessment/Assessment 3/Quiz App/src/components/Quiz.jsx
@@ -0,0 +1,44 @@
+import { useState, useCallback} from "react";
+import QUESTIONS from '../questions';
+import QuestionTimer from "./QuestionTimer";
+import Answers from "./Answers";
+import Question from "./Question";
+import quizCompleteImg from '../assets/quiz-complete.png';
+
+export default function Quiz(){
+    const [userAnswers, setUserAnswers] = useState([]);
+    
+    const activeQuestionIndex = userAnswers.length; 
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+    const handleSelectAnswer = useCallback(
+        function handleSelectAnswer(selectedAnswer){
+               
+            setUserAnswers((prevUsersAnswers) =>{
+                return [...prevUsersAnswers, selectedAnswer];
+            });     
+    },[]);
+
+    const handleSkipAnswer = useCallback(()=> handleSelectAnswer(null), [handleSelectAnswer]);
+
+    if(quizIsComplete){
+        return(
+            <div id="summary">
+                <img src={quizCompleteImg} alt="Trophy icon" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        );
+    }
+    
+    return(
+        <div id="quiz">
+            <Question
+                key = {activeQuestionIndex}
+                index = {activeQuestionIndex}
+                onSelectAnswer = {handleSelectAnswer}
+                onSkipAnswer ={handleSkipAnswer}
+            
+            />
+        </div>
+    );
+} 
